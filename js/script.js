39670b27bd1596b920deb63349a6e78ae9b7ad0c
suzuki-kotaro <59900492+suzuki-kotaro@users.noreply.github.com>
2020-01-26 14:10:41 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-$(function(){
-    
-});
+jQuery(function ($) {
+      $(function () {
+        $('a[href^="#"]').click(function () {
+          var speed = 1000;
+          var href = $(this).attr("href");
+          var target = $(href == "#" || href == "" ? 'html' : href);
+          var position = target.offset().top;
+          if ($('#menu-switch').is(':checked')) { //全選択・全解除がcheckedだったら
+            $('#menu-switch').prop('checked', false); //アイテムを全部checkedにする
+          }
+          $("html, body").animate({
+            scrollTop: position
+          }, speed, "swing");
+          return false;
+        });
+      });
